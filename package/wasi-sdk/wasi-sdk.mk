b39a5387ceb52e0c85a29cc661987a871286c28c
Grzegorz Placzek <gplaczek@antmicro.com>
2023-11-06 16:36:46 +0100

    [#50880] Fix wasi-sdk timestamps

--- package/wasi-sdk/wasi-sdk.mk
+++ package/wasi-sdk/wasi-sdk.mk
@@ -6,9 +6,11 @@ WASI_SDK_PATH := $(WASI_SDK_SRC_DIR)
 
 $(WASI_SDK_SRC_TAR):
 	@wget -qO $(WASI_SDK_SRC_TAR) https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-$(WASI_SDK_VERSION)/wasi-sdk-$(WASI_SDK_VERSION_FULL)-linux.tar.gz
+	@touch $(WASI_SDK_SRC_TAR)
 
 $(WASI_SDK_SRC_DIR): $(WASI_SDK_SRC_TAR)
-	@tar xf $(WASI_SDK_SRC_TAR) -C $(BUILD_DIR)
+	@tar -xf $(WASI_SDK_SRC_TAR) -C $(BUILD_DIR)
+	@touch $(WASI_SDK_SRC_DIR)
 
 .PHONY: WASI_SDK
 WASI_SDK: $(WASI_SDK_SRC_DIR)
