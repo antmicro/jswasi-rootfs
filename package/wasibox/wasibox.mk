eadbd78ba1b42adaa8ca8dc1c7e378412cb97ebf
Grzegorz Placzek <gplaczek@antmicro.com>
2024-02-15 14:34:01 +0100

    [#42406] Bump jswasi and wasibox

--- package/wasibox/wasibox.mk
+++ package/wasibox/wasibox.mk
@@ -1,7 +1,7 @@
 WASIBOX_DEPENDENCIES := RUST WASI_SDK
 WASIBOX_PKG_NAME := wasibox
 
-WASIBOX_SRC_REV := bc3b80029fa533beec113416cfe6d446ee3c352b
+WASIBOX_SRC_REV := b31b9309f8bc8929c628fb245bc5e5b56a07b472
 WASIBOX_SRC_URL := $(call github_url,antmicro,wasibox,$(WASIBOX_SRC_REV))
 
 WASIBOX_TARGET = $(WASIBOX_SRC_DIR)/target/wasm32-wasi/release/wasibox.wasm
