60064d1f08b93935192e270f05a65ce22344288f
Grzegorz Placzek <gplaczek@antmicro.com>
2024-07-23 10:02:05 +0200

    [#61992] Bump wasibox and jswasi

--- package/wasibox/wasibox.mk
+++ package/wasibox/wasibox.mk
@@ -1,7 +1,7 @@
 WASIBOX_DEPENDENCIES := RUST WASI_SDK
 WASIBOX_PKG_NAME := wasibox
 
-WASIBOX_SRC_REV := 03e6a1d72e6b197d77519bd2ef31fc7220af25fb
+WASIBOX_SRC_REV := ea5d990e9ed19f3756af006632264c9f1e15a298
 WASIBOX_SRC_URL := $(call github_url,antmicro,wasibox,$(WASIBOX_SRC_REV))
 
 WASIBOX_ROOTFS_PATH := /usr/bin/wasibox
@@ -19,6 +19,7 @@ define WASIBOX_INSTALL_CMDS_EXTRA
 	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/stty
 	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/tar
 	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/wget
+	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/mknod
 endef
 
 $(eval $(call get-sources,WASIBOX))
