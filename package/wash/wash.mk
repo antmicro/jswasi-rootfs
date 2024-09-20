a0bf15cd58575f64026dbedd539659d925f8afc4
Grzegorz Placzek <gplaczek@antmicro.com>
2024-09-20 11:02:08 +0200

    [#65986] Bump wash

--- package/wash/wash.mk
+++ package/wash/wash.mk
@@ -1,7 +1,7 @@
 WASH_DEPENDENCIES := RUST WASI_SDK
 WASH_PKG_NAME := wash
 
-WASH_SRC_REV := 225ead9ea183987041b1a901057dd4afd8b4f829
+WASH_SRC_REV := e1142499bd330be6d3be1fdd522500b77c66457f
 WASH_SRC_URL := $(call github_url,antmicro,wash,$(WASH_SRC_REV))
 
 WASH_ROOTFS_PATH := /usr/bin/wash
