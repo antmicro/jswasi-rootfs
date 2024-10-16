fa780ba8aa3600a708ca3eaca14ce078fe479359
Krzysztof Juszczyk <kjuszczyk@antmicro.com>
2024-10-16 11:34:51 +0200

    [#59227] Bump wash commit

--- package/wash/wash.mk
+++ package/wash/wash.mk
@@ -1,7 +1,7 @@
 WASH_DEPENDENCIES := RUST WASI_SDK
 WASH_PKG_NAME := wash
 
-WASH_SRC_REV := e1142499bd330be6d3be1fdd522500b77c66457f
+WASH_SRC_REV := c75120b45e3c2407adf0a4fa6ce92f00b670db70
 WASH_SRC_URL := $(call github_url,antmicro,wash,$(WASH_SRC_REV))
 
 WASH_ROOTFS_PATH := /usr/bin/wash
