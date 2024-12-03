d7a5eaee3a7041c6ce9822edcd8aab5771ae5c8b
Grzegorz Placzek <gplaczek@antmicro.com>
2024-12-03 22:46:10 +0100

    [#69704] Set working directory in jq

--- package/jq/jq.mk
+++ package/jq/jq.mk
@@ -1,4 +1,4 @@
-JQ_DEPENDENCIES := WASI_SDK
+JQ_DEPENDENCIES := WASI_SDK WASI_EXT_LIB
 JQ_PKG_NAME := jq
 
 JQ_SRC_REV := 585570cb5c8e6a514b9e4b6a419dbe0d9c80f3bb
@@ -8,10 +8,12 @@ JQ_SRC_DIR := $(BUILD_DIR)/jq-$(JQ_SRC_REV)
 JQ_BUILD := $(JQ_SRC_DIR)/jq
 JQ_DIST := $(ROOTFS_DIR)/usr/bin/jq
 
-$(JQ_BUILD): | $(JQ_SRC_DIR) $(JQ_DEPENDENCIES)
+JQ_PATCHES := $(wildcard $(PACKAGE_DIR)/jq/*.patch)
+
+$(JQ_BUILD): JQ_PATCH | $(JQ_SRC_DIR) $(JQ_DEPENDENCIES)
 	export CC="$(WASI_SDK_PATH)/bin/clang" && \
-	export CFLAGS="-O2 -D_WASI_EMULATED_SIGNAL $(CFLAGS)" && \
-	export LDFLAGS="$(LDFLAGS) -lwasi-emulated-signal" && \
+	export CFLAGS="-O2 -D_WASI_EMULATED_SIGNAL -I $(WASI_EXT_LIB_INLUDE_PATH) $(CFLAGS)" && \
+	export LDFLAGS="$(LDFLAGS) -L$(WASI_EXT_LIB_LD_PATH) -lwasi_ext_lib -lwasi-emulated-signal" && \
 	cd $(JQ_SRC_DIR) && \
 	autoreconf -i && \
 	./configure --host=wasm32-wasi --target=wasm32-wasi --disable-docs --disable-valgrind --disable-maintainer-mode --with-onigurama=builtin --prefix=/usr/local && \
@@ -22,4 +24,6 @@ $(JQ_DIST): $(JQ_BUILD) | $(ROOTFS_DIR)
 
 .PHONY: JQ
 JQ: $(JQ_DIST)
+
 $(eval $(call get-sources-git,JQ))
+$(eval $(call apply-patches,JQ))
