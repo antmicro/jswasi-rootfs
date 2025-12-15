756392b2e79d5bd1dca6813108de603f27fbbbea
Dawid Kaczmarzyk <dkaczmarzyk@internships.antmicro.com>
2025-12-15 17:49:41 +0100

    [#88541] Bump wash (command substitution fixed)

--- package/wash/wash.mk
+++ package/wash/wash.mk
@@ -1,7 +1,7 @@
 WASH_DEPENDENCIES := RUST WASI_SDK
 WASH_PKG_NAME := wash
 
-WASH_SRC_REV := 56dca88af5bd2e0f6cc923e1af80c613eb6ada80
+WASH_SRC_REV := 8e32c14a993c0c39be7c7d20655f0536ef9272c2
 WASH_SRC_URL := $(call github_url,antmicro,wash,$(WASH_SRC_REV))
 
 WASH_ROOTFS_PATH := /usr/bin/wash
