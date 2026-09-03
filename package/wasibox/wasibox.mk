3591bf4d76a3da19f664c4b0fdf8cb26206fee94
Dawid Kaczmarzyk <dkaczmarzyk@internships.antmicro.com>
2026-09-03 14:41:50 +0200

    [#102405] Conditionally install wasibox symlinks

--- package/wasibox/wasibox.mk
+++ package/wasibox/wasibox.mk
@@ -7,26 +7,15 @@ WASIBOX_SRC_URL := $(call github_url,antmicro,wasibox,$(WASIBOX_SRC_REV))
 WASIBOX_ROOTFS_PATH := /usr/bin/wasibox
 WASIBOX_DIST := $(ROOTFS_DIR)$(WASIBOX_ROOTFS_PATH)
 
-define WASIBOX_INSTALL_CMDS_EXTRA
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/imgcat
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/purge
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/tree
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/unzip
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/hexdump
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/kill
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/mount
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/umount
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/stty
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/tar
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/websocat
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/wget
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/mknod
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/uname
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/free
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/ps
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/reset
-	ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/reload
+WASIBOX_INSTALL_SYMLINKS ?= 1
+WASIBOX_APPLETS := imgcat purge tree unzip hexdump kill mount umount stty tar websocat wget mknod uname free ps reset reload
 
+define WASIBOX_INSTALL_CMDS_EXTRA
+	if [ "$(WASIBOX_INSTALL_SYMLINKS)" = "1" ]; then \
+		for applet in $(WASIBOX_APPLETS); do \
+			ln -fs $(WASIBOX_ROOTFS_PATH) $(ROOTFS_DIR)/usr/bin/$$$$applet; \
+		done; \
+	fi
 	mkdir -p $(ROOTFS_DIR)/etc/init.d
 endef
 
